2a69cbf33b63066c7143bef4e7e85fc26b60ce3c
iratxetos <iratxetxu@gmail.com>
2021-09-17 13:10:34 +0200

    created project

---
+++ src/components/App.js
@@ -0,0 +1,233 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page">
+        <header className="form__header">
+          <a href="./index.html" alt="home" title="Home">
+            <img
+              className="form__header--image"
+              src="./assets/images/tarjetas-molonas.svg"
+              alt="Awesome cards"
+              srcset=""
+            /></a>
+        </header>
+
+        <main className="card">
+          <section className="photo__container">
+            <div className="photo__reset--container">
+              <ul className="photo__reset--menu">
+                <li className="photo__reset--text">
+                  <i className="far fa-trash-alt"></i>
+                </li>
+                <li className="photo__reset--text">Reset</li>
+              </ul>
+            </div>
+            <div className="photo__card--container js-photo">
+              <div class="photo__card--rectangle"></div>
+              <p class="photo__card--name">Nombre Apellido</p>
+              <p class="photo__card--frontend">Front-end developer</p>
+              <div class="photo__card--photo" id="photo"></div>
+              <div class="photo__card--rrss">
+                <a href="" id="telLink" target="_blank">
+                  <i class="fas fa-mobile-alt photo__card--rrss-icon"></i
+                  ></a>
+                <a href="" id="emailLink" target="_blank"
+                ><i class="far fa-envelope photo__card--rrss-icon"></i
+                ></a>
+                <a href="" id="linkedinLink" target="_blank"
+                ><i class="fab fa-linkedin-in photo__card--rrss-icon"></i
+                ></a>
+                <a href="" id="githubLink" target="_blank"
+                ><i class="fab fa-github-alt photo__card--rrss-icon"></i
+                ></a>
+              </div>
+            </div>
+          </section>
+          <form className="form" action="">
+            <legend className="design__legend">
+              <div className="form__parameters">
+                <h2 className="form__title"><i className="far fa-object-ungroup form__icon"></i>Diseña</h2>
+                <i className="fas fa-chevron-down design__arrow form__arrow js__collapsable"></i>
+              </div>
+
+
+              <fieldset className="hidden design__check js__fieldset">
+                <label className="design__check--colors" htmlFor="color">colores</label>
+
+                <section className="design__check--section">
+                  <input
+                    className="js-palette-1"
+                    type="radio"
+                    id="palette"
+                    name="color"
+                    value="1"
+                  />
+
+                  <section className="design__check--section__box">
+                    <div className="box box__1"></div>
+                    <div className="box box__2"></div>
+                    <div className="box box__3"></div>
+                  </section>
+                </section>
+
+                <section className="design__check--section">
+                  <input
+                    className="js-palette-2"
+                    type="radio"
+                    id="palette"
+                    name="color"
+                    value="2"
+                  />
+
+                  <section className="design__check--section__box">
+                    <div className="box box__4"></div>
+                    <div className="box box__5"></div>
+                    <div className="box box__6"></div>
+                  </section>
+                </section>
+                <section className="design__check--section">
+                  <input
+                    className="js-palette-3"
+                    type="radio"
+                    id="palette"
+                    name="color"
+                    value="3"
+                  />
+
+                  <section className="design__check--section__box">
+                    <div className="box box__7"></div>
+                    <div className="box box__8"></div>
+                    <div className="box box__9"></div>
+                  </section>
+                </section>
+                <hr />
+              </fieldset>
+              <hr className="form__line" />
+            </legend>
+
+            <legend className="fill">
+              <div className="form__parameters">
+                <h2 className="form__title"><i className="far fa-keyboard form__icon"></i>Rellena</h2>
+                <i className="fas fa-chevron-down fill__arrow form__arrow js__collapsable"></i>
+              </div>
+
+              <fieldset className="hidden fill__form js__fieldset">
+                <label className="fill__form--label" htmlFor="name"> Nombre Completo</label>
+                <input
+                  className="fill__form--input name"
+                  id="fullname"
+                  type="text"
+                  placeholder=" Ej: Sally Jill"
+                  required
+                />
+
+                <label className="fill__form--label" htmlFor="job"> Puesto</label>
+                <input
+                  className="fill__form--input job"
+                  id="job"
+                  type="text"
+                  placeholder=" Ej: Front-end unicorn"
+                  required
+                />
+
+                <label className="fill__form--label" htmlFor="photo photo__square">
+                  Imagen de perfil</label
+                >
+
+                <label htmlFor="imgselector" className="fill__form--input photo"
+                >Añadir imagen
+                  <input
+                    type="file"
+                    id="imgselector"
+                    name="imgselector"
+                    className="hide--input js__input fill__form--input photo"
+                  />
+                </label>
+                { /* <button type='button' className='fill__form--input photo'>
+
+                </button>   */}
+
+                <div className="fill__form--input photo__square">
+                  { /* <img className='square' /> */}
+                </div>
+
+                <label className="fill__form--label" htmlFor="email"> Email</label>
+                <input
+                  className="fill__form--input email"
+                  id="emailFill"
+                  type="email"
+                  placeholder=" Ej: sally-hill@gmail.com"
+                  required
+                />
+
+                <label className="fill__form--label" htmlFor="tel"> Teléfono</label>
+                <input
+                  className="fill__form--input tel"
+                  id="telFill"
+                  type="tel"
+                  placeholder=" Ej: 555-55-55-55"
+                // ^[0-9]{9}$
+                />
+
+                <label className="fill__form--label" htmlFor="linkedin"> Linkedin</label>
+                <input
+                  className="fill__form--input linkedin"
+                  id="linkedinFill"
+                  type="text"
+                  placeholder=" Ej: linkedin.com/in/sally.hill"
+                />
+
+                <label className="fill__form--label" htmlFor="github"> Github</label>
+                <input
+                  className="fill__form--input github"
+                  id="githubFill"
+                  type="text"
+                  placeholder=" Ej: @sally-hill"
+                />
+              </fieldset>
+              <hr className="form__line" />
+            </legend>
+
+            <legend className="share">
+              <div className="form__parameters">
+                <h2 className="form__title"><i className="fas fa-share-alt form__icon"></i>Comparte</h2>
+                <i className="fas fa-chevron-down share__arrow form__arrow js__collapsable"></i>
+              </div>
+
+              <fieldset className="hidden dropdown js__fieldset">
+                <button className="dropdown__button--create">
+                  <i className="far fa-address-card"></i> Crear tarjeta
+                </button>
+
+                {/*  Aquí iría html de cuando se crea la tarjeta
+                <p className='dropdown__message'>La tarjeta ha sido creada</p>
+                <button className='dropdown__button--tw'>
+                  <i className='fab fa-twitter'></i>Compartir en twitter
+                </button>
+              </div>  */}
+              </fieldset>
+              <hr className="form__line" />
+            </legend>
+
+          </form>
+        </main>
+
+        <footer className="footer">
+          <h6 className="footer__copyright">ochoPinocho for awesome profile-cards @2021</h6>
+          <img
+            className="footer__image"
+            src="./assets/images/logo-adalab.png"
+            alt="logo Adalab"
+            title="Logo de Adalab"
+          />
+        </footer>
+
+      </div>
+    </div >
+  );
+}
+
+export default App;
