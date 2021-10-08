0e07484894a597e4447f15e6ff02f0b054fee274
taniamg <taniamg81@gmail.com>
2021-10-09 01:08:09 +0200

    upload without changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -206,57 +206,7 @@ const Form = (props) => {
         </legend>
 
         {/* FORMULARIO - COMPARTE*/}
-        <legend className="share">
-          <div
-            className="form__parameters"
-            id="share"
-            onClick={props.handleCollapsable}
-          >
-            <h2 className="form__title">
-              <i className="fas fa-share-alt form__icon"></i>Comparte
-            </h2>
-            <i
-              className={`fas fa-chevron-down share__arrow form__arrow js__collapsable ${props.arrowShare}`}
-            ></i>
-          </div>
-
-          <fieldset
-            className={` dropdown js__fieldset ${props.collapsableShare}`}
-          >
-            <button
-              className="dropdown__button--create"
-              onClick={props.handleShareCard}
-            >
-              <i className="far fa-address-card"></i> Crear tarjeta
-            </button>
-            <p className="legal">Al pulsar Crear tarjeta autorizo a Adalab a compartir mis datos personales y usarlos para fines promocionales</p>
-            <div
-              className={`share__section--done  ${props.collapsableShareLink}`}
-            >
-              <h2 className="share__section--done__text js_undone">
-                La tarjeta ha sido creada:
-              </h2>
-              <a
-                className="share__section--done__link js_url"
-                href=" "
-                target="_blank"
-              >{props.URLdata}
-              </a>
-
-              <button className="share__section--done__button js_undone2">
-                <a
-                  className="share__section--done__button--link js_twitter_link"
-                  href=" "
-                  target="_blank"
-                >
-                  <i className="icon3 fab fa-twitter"></i> &nbsp; Compartir en
-                  twitter
-                </a>
-              </button>
-            </div>
-          </fieldset>
-          <hr className="form__line" />
-        </legend>
+        
         <Share data={props.data} />
       </form>
     </div>
