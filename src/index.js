522065f0820d311459aa399052e01c7e652cffbc
taniamg <taniamg81@gmail.com>
2021-10-18 13:05:50 +0200

    upload paint card

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 const express = require("express");
 const cors = require("cors");
 const dataBase = require("better-sqlite3");
-const db = dataBase("./src/cards.db", {
+const { response } = require("express");
+const db = dataBase("./src/data/cards.db", {
   verbose: console.log
 })
 
@@ -13,8 +14,8 @@ const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json());
-server.use('view engine', 'ejs');
+
+server.set('view engine', 'ejs');
 server.use( express.json({limit:'10mb'}));
 
 // Arrancamos el servidor en el puerto 3000
@@ -26,21 +27,21 @@ server.listen(serverPort, () => {
 const staticServerPath = "./src/public-react";
 server.use(express.static(staticServerPath));
 //endpoint paint data
-server.get('/data/:id', (req, res)=>{
+server.get('/card/:id', (req, res)=>{
   const id= req.params.id;
   const query =db.prepare(' SELECT * FROM cards WHERE id = ?');
   const data = query.get(id);
-  if (data != null){
-    res.render ('card',data);
+  if (data === null){
+    res.render ('card not found');
   }else{
-    res.render ('notFound');
+
   }
 })
 
 
 
 // Escribimos los endpoints que queramos
-server.get("/card", (req, res) => {
+server.post("/card", (req, res) => {
   const fields = [];
 
   if (req.body.phone === "") {
@@ -72,8 +73,11 @@ server.get("/card", (req, res) => {
 
   const response = {};
   if (fields.length === 0) {
-    response.cardURL = "http://localhost:4001/card/";
-    response.success = true;
+    const query= db.prepare ('INSERT INTO cards (palette, name, job, photo, email, phone, linkedin, github) VALUES (?,?,?,?,?,?,?,?)');
+    const result =query.run (req.body.palette, req.body.name, req.body.job, req.body.photo, req.body.email, req.body.phone, req.body.linkedin, req.body.github)
+    response.success= true;
+    response.cardURL = `http://localhost:4001/card/${result.lastInsertRowid}`;
+    console.log(result);
   } else {
     response.error = fields;
     response.success = false;
